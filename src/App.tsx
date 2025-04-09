4180096a4056ffad9e387b134170ce87edd67490
Todd Brannon <toddqbrannon@gmail.com>
2025-04-09 14:55:03 -0500

    Update App.tsx to show dynamic copyright date

--- src/App.tsx
+++ src/App.tsx
@@ -354,9 +354,10 @@ function HomePage() {
                 </ul>
               </div>
             </div>
-            <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-              <p>&copy; 2024 TruSponse Solutions. All rights reserved.</p>
-            </div>
+           <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
+            <p>&copy; {new Date().getFullYear()} TruSponse Solutions. All rights reserved.</p>
+          </div>
+
           </div>
         </footer>
       </div>
